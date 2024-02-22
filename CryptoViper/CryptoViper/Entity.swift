7efec0a688d8461a551889939d308c8a44b5a807
mef25 <148797367+mef25@users.noreply.github.com>
2024-02-22 15:30:25 +0300

    Proje Dosyaları

---
+++ CryptoViper/CryptoViper/Entity.swift
@@ -0,0 +1,15 @@
+//
+//  Entity.swift
+//  CryptoViper
+//
+//  Created by Mehmet Emin Fırıncı on 19.02.2024.
+//
+
+import Foundation
+
+// Struct
+
+struct Crypto : Decodable {
+    let currency : String
+    let price : String
+}
